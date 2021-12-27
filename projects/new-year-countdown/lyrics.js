237d8a29fcd725c4dba367b0a50670e17e0a4dfc
Naz <bluspringindohq@gmail.com>
2021-12-28 05:22:13 +0800

    Updated website entirely

---
+++ projects/new-year-countdown/lyrics.js
@@ -0,0 +1,14 @@
+const LYRICS = {
+    'Technoblade - Blitz': [
+        
+    ],
+    'Wilbur Soot - Your New Boyfriend': [
+
+    ],
+    'Glass Animals - Heat Waves': [
+
+    ],
+    'Lovejoy - The Fall': [
+        
+    ]
+};
