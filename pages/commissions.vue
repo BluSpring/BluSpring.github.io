60e0aa96815b15c3301e40f8d1ff84564f9f5b76
Naz <bluspringindohq@gmail.com>
2022-05-24 01:02:29 +0800

    Laziness

---
+++ pages/commissions.vue
@@ -0,0 +1,96 @@
+<template>
+    <main class="w-full h-full m-auto absolute top-0 left-0 table">
+        <h3 class="text-center text-white text-5xl pt-14">Currently a work in progress, please come back later!</h3>
+    </main>
+</template>
+
+<!-- Will be completed later. -->
+<!--<template>
+    <main class="w-full h-full m-auto absolute top-0 left-0 table">
+        <div class="commission-types">
+            <section id="3d">
+                <header>
+                    3D-based Commissions
+                </header>
+
+                <div class="cards">
+                    <CommissionCard 
+                        name="3D Minecraft Animation"
+                        
+                        description="I will make a 3D animation of your Minecraft character(s) in whatever way you wish, as long as it's following the terms."
+
+                        slots="5"
+                        max-slots="5"
+                        price="$10 / 15s ($0.67 / 1s)"
+                    />
+                </div>
+            </section>
+
+            <section id="dev">
+                <header>
+                    Programming-based Commissions
+                </header>
+
+                <div class="cards">
+                    <CommissionCard
+                        name="Minecraft Plugin"
+                        description="I can create a Minecraft plugin for you, as long as it's following the terms, and as long as it is possible in Minecraft in the first place."
+
+                        slots="5"
+                        max-slots="5"
+                        price="$26 / plugin"
+                    />
+
+                    <CommissionCard
+                        name="Minecraft Mod"
+                        description="I can create a Minecraft mod for you, as long as it's following the terms, and depending on its complexity."
+
+                        slots="5"
+                        max-slots="5"
+                        price="$9 / hour of work"
+                    />
+                </div>
+            </section>
+        </div>
+
+        <div class="terms">
+            <h2>Terms of Service</h2>
+            <h4>By commissioning me, you are agreeing to these terms of service.</h4>
+
+            <p>
+                <strong>General Information</strong>
+
+                1. No NSFW in any way is allowed.
+
+                2. Once the commission is paid for, I shall place your commission in a queue to be finished.
+
+                3. I reserve the right to decline any commission request.
+
+                4. Any personal information given will be kept private and confidential.
+
+
+                <strong>Usage and Rights</strong>
+
+                1. I do not allow my work to be minted as Non-Fungible Tokens (NFTs).
+
+                2. You are not allowed to claim the commissioned work as your own.
+
+                3. Commissioned works must not be used for profit, with videos and Minecraft servers being exceptions, unless stated in an agreement.
+
+                4. I reserve the right to choose the completed commissions for promotion or as examples for myself, however you may state if you do not wish for this 
+                to be the case.
+                
+
+                <strong>Cancellations and Refunds</strong>
+
+                1. At any time, I reserve the right to cancel any unpaid commissions, cancel and refund paid commissions, or put commissions on hold as a result of 
+                poor communication, rude or inappropriate behaviour, or any kind of abuse. This right will also apply if I have encountered any serious problems
+                regarding my personal life, health or equipment.
+
+                2. Commissioners will be notified prior to cancellation.
+
+                3. Commissioners may cancel a work in progress, or withdraw from their place in the queue, but they will not be granted a refund.
+            </p>
+        </div>
+    </main>
+</template>-->
