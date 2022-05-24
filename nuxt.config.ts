09a2a1d000172b643235354941cdfba2b65c4800
Naz <bluspringindohq@gmail.com>
2022-05-25 00:45:52 +0800

    ........bro?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,4 @@
 import { defineNuxtConfig } from 'nuxt'
-import RollupNodeResolve from '@rollup/plugin-node-resolve';
-import RollupTS from 'rollup-plugin-typescript';
-import RollupImport from 'rollup-plugin-import-resolver';
-import RollupCSS from 'rollup-plugin-import-css';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
@@ -20,14 +16,5 @@ export default defineNuxtConfig({
     modules: [
         '@nuxtjs/tailwindcss'
     ],
-    nitro: {
-        rollupConfig: {
-            plugins: [
-                RollupCSS(),
-                RollupNodeResolve(),
-                RollupTS(),
-                RollupImport(),
-            ]
-        }
-    }
+    ssr: false
 })
